f5221a86c6a91f76e458a9a5d8a95c64d91e0976
axltavideus <yeetna01@gmail.com>
2025-03-06 20:46:53 +0700

    perbaikin migrate

--- database/migrations/2025_03_06_133203_create_games_table.php
+++ database/migrations/2025_03_06_133203_create_games_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('games');
             $table->string('category');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('slug');
             $table->string('content');
             $table->timestamps();
